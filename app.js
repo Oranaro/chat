a464ca615b4a4d96a1cb3fe9a6120fcd698569fc
Christophe Sikes <sikes_c@etna-alternance.net>
2021-04-17 14:07:55 +0200

    test with commands data

--- app.js
+++ app.js
@@ -4,13 +4,16 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+var redis = require('async-redis').createClient(process.env.REDIS_URL);
+
 const PagesController = require('./controllers/pages.controller');
+new PagesController.pages(redis)
 // View engine
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 // Controllers (routes)
-app.use('/', PagesController);
+app.use('/', PagesController.router);
 
 app.listen(port, () => {
 	console.log(`Server on : ${port}/`);
--- controllers/pages.controller.js
+++ controllers/pages.controller.js
@@ -2,6 +2,31 @@ const express = require('express');
 const router = express.Router();
 const { getDoc, buildRecipesArray } = require('../libs/sheets.js');
 
+// var redis = require('async-redis').createClient(process.env.REDIS_URL);
+
+function pages(redis) {
+	router.get('/commande', (req, res) => {
+		var context = {commands:[], descriptedCommands:[]}
+	
+		redis.hgetall("commands").then(all =>{
+			for(var cmd in all){
+				context.commands.push({name: cmd, description: all[cmd]})
+			}
+	
+			redis.hgetall("commands/description").then(descAll =>{
+				for(var descCmd in descAll){
+					context.descriptedCommands.push({name: descCmd, description: descAll[descCmd]})
+				}
+				res.render('pages/commande', {
+					title: 'Commandes',
+					style: 'style1.css',
+					context,
+				});			
+			})
+		})
+	});
+}
+
 router.get('/', (req, res) => {
 	res.render('pages/home', {
 		title: 'Chat Des Bois',
@@ -18,19 +43,14 @@ router.get('/classement', (req, res) => {
 
 router.get('/cuisine', async (req, res) => {
 	const document = await getDoc();
-
 	const recipes = buildRecipesArray(document);
+	console.log(recipes)
 
 	res.render('pages/cuisine', {
 		title: 'Recettes',
 		style: 'style1.css',
-		recipes: recipes,
+		recipes,
 	});
 });
-router.get('/commande', (req, res) => {
-	res.render('pages/commande', {
-		title: 'Commandes',
-		style: 'style1.css',
-	});
-});
-module.exports = router;
+
+module.exports = { pages, router };
